24145d32cd9f4fcfc452c1695bf7ed08ecf8d766
jhonaln <jhonaln@gmail.com>
2024-12-08 20:29:28 -0500

    Cambios en la pantalla product

--- app/screens/Product.js
+++ app/screens/Product.js
@@ -1,23 +1,36 @@
-import {View, Text, StyleSheet, Button} from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-export const Product = ({navigation}) =>{
-    return <View style={styles.container}> 
-        <Text>Estas en productos</Text>
-        <Button
-            title='HOME'
-            onPress={()=>{
-                navigation.navigate('HomeNav');
-            }}
-        />
+export const Product = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Bienvenidos a mi tienda, soy Jhon</Text>
+      <Text style={styles.producto1}>producto 1</Text>
+      <Text style={styles.producto2}>producto 2</Text>
+      <Text style={styles.producto3}>producto 3</Text>
+      <Button
+        title="HOME"
+        onPress={() => {
+          navigation.navigate('HomeNav');
+        }}
+      />
     </View>
-
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  producto1: {
+    color: 'red',
+  },
+  producto2: {
+    color: 'green',
+  },
+  producto3: {
+    color: 'blue',
+  },
+});
