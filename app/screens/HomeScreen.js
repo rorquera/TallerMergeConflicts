d372acf404e5c756fa1f67b624087ab2c8747abb
rorquera <92764410+rorquera@users.noreply.github.com>
2024-12-08 19:47:28 -0500

    cambio color boton, fondo pantalla y cambios en los textos

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -2,7 +2,8 @@ import { View, Text, StyleSheet, Button } from 'react-native'
 
 export const Home = ({ navigation }) => {
     return <View style={styles.container}>
-        <Text>HOME</Text>
+        <Text>Bienvenido a mi aplicación, soy Rolando</Text>
+        <Text>Estoy en el Home</Text>
 
         <View style={styles.botones}>
 
@@ -12,6 +13,7 @@ export const Home = ({ navigation }) => {
                     onPress={() => {
                         navigation.navigate('ContactsNav');
                     }}
+                    color='red'
                 />
             </View>
 
@@ -33,7 +35,7 @@ export const Home = ({ navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: 'lightgray',
         alignItems: 'center',
         justifyContent: 'center',
     },
